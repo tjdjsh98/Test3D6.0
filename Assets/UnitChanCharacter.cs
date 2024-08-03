c8d3d32240aed3ad683f9f7cd5d0255da4dec39b
tjdjsh98 <69230675+tjdjsh98@users.noreply.github.com>
2024-08-03 20:58:55 +0900

    움직임 추가

--- Assets/UnitChanCharacter.cs
+++ Assets/UnitChanCharacter.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UnitChanCharacter : MonoBehaviour
 {
+    [SerializeField]Camera _camera;
+    GameObject _model;
+    CapsuleCollider _collider;
     Rigidbody _rigidBody;
     Animator _animator;
 
     float _maxSpeed = 5f;
+
+    bool _isContactWall;
+    bool _isContactGround;
+
     private void Awake()
     {
+        _model = transform.Find("Model").gameObject;
+        _collider = GetComponent<CapsuleCollider>();
         _rigidBody =GetComponent<Rigidbody>();
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
         _rigidBody.maxLinearVelocity = _maxSpeed;
     }
 
@@ -18,19 +28,39 @@ public class UnitChanCharacter : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + Vector3.down*0.2f);
 
+        if(_collider == null)
+            _collider = GetComponent<CapsuleCollider>();
+        Gizmos.DrawLine(gameObject.transform.position + _collider.center, gameObject.transform.position + _collider.center +transform.forward*0.3f);
+
     }
     void Update()
     {
         ControlMovement();
         if(Physics.Raycast(transform.position, Vector3.down, 0.2f, LayerMask.GetMask("Ground")))
         {
+            _isContactGround= true;
             _animator.SetBool("ContactGround", true);
         }
         else
         {
+            _isContactGround = false;
             _animator.SetBool("ContactGround", false);
         }
+
+        if (Physics.Raycast(transform.position + _collider.center, _collider.center + transform.forward , 0.3f, LayerMask.GetMask("Ground")))
+        {
+            _rigidBody.useGravity = false;
+            _isContactWall = true;
+            _animator.SetBool("ContactWall", true);
+        }
+        else
+        {
+            _rigidBody.useGravity = true;
+            _isContactWall = false;
+            _animator.SetBool("ContactWall", false);
+        }
     }
+
 
     void ControlMovement()
     {
@@ -38,38 +68,64 @@ public class UnitChanCharacter : MonoBehaviour
 
         if(Input.GetKey(KeyCode.W))
         {
-            moveDirection += Vector3.forward;
+            Vector3 dir = _camera.transform.forward;
+            dir.y = 0;
+            dir.Normalize();
+            moveDirection += dir;
+
         }
         if (Input.GetKey(KeyCode.S))
         {
-            moveDirection += Vector3.back;
+            Vector3 dir = _camera.transform.forward;
+            dir.y = 0;
+            dir.Normalize();
+            moveDirection -= dir;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            moveDirection += Vector3.left;
+            Vector3 dir = _camera.transform.right;
+            dir.y = 0;
+            dir.Normalize();
+            moveDirection -= dir;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            moveDirection += Vector3.right;
+            Vector3 dir = _camera.transform.right;
+            dir.y = 0;
+            dir.Normalize();
+            moveDirection += dir;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (_isContactGround)
         {
-            _rigidBody.AddForce(Vector3.up * 1000f,ForceMode.Impulse);
-            
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidBody.AddForce(Vector3.up * 1000f, ForceMode.Impulse);
 
-        if (moveDirection != Vector3.zero)
+            }
+
+            if (moveDirection != Vector3.zero)
+            {
+                float moveAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+                float deltaAngle = Mathf.DeltaAngle(_model.transform.rotation.eulerAngles.y, moveAngle) * 0.2f;
+
+                _model.transform.rotation = Quaternion.Euler(0, _model.transform.rotation.eulerAngles.y + deltaAngle, 0);
+
+                _rigidBody.AddForce(moveDirection * 5000);
+            }
+
+            _animator.SetFloat("Velocity", Mathf.Abs(_rigidBody.linearVelocity.x) + Mathf.Abs(_rigidBody.linearVelocity.z));
+            _animator.SetFloat("VelocityY", _rigidBody.linearVelocity.y);
+        }
+        if(_isContactWall)
         {
-            float moveAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-            float deltaAngle = Mathf.DeltaAngle(transform.rotation.eulerAngles.y, moveAngle)*0.2f;
+            _animator.SetFloat("MoveWall", moveDirection.z);
+            _rigidBody.linearVelocity = Vector3.up * moveDirection.z;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidBody.AddForce(Vector3.up + -_model.transform.forward * 1000f, ForceMode.Impulse);
 
-            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + deltaAngle, 0);
-
-            _rigidBody.AddForce(moveDirection* 500);
+            }
         }
-
-        _animator.SetFloat("Velocity", Mathf.Abs(_rigidBody.linearVelocity.x )+ Mathf.Abs(_rigidBody.linearVelocity.z));
-        _animator.SetFloat("VelocityY", _rigidBody.linearVelocity.y);
     }
 }
